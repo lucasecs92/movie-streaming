545d29a757fa2fbc0155d958ade4c9afc07da873
Lucas Eduardo <lucased18@gmail.com>
2025-03-11 23:09:48 -0300

    feat
    Adiciona navegação à direita com links para "Login" e "Cadastro"
    Adiciona seção principal
    Estrutura o rodapé da página

--- streaming-root/next.config.mjs
+++ streaming-root/next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const nextConfig = {
+    sassOptions: {
+        additionalData: `$var: red;`,
+    },
+};
 
 export default nextConfig;
