c63538e2f0960185bc39d5515192f6077031ee19
Lucas Eduardo <lucased18@gmail.com>
2025-04-25 15:46:36 -0300

    style
    ajustando responsividade dos modal

--- streaming-root/src/components/ModalCadastro.jsx
+++ streaming-root/src/components/ModalCadastro.jsx
@@ -50,62 +50,62 @@ export default function ModalCadastro({
         <h1 onClick={handleHomeClick}>Cineminha</h1>
         <span onClick={handleLoginClick}>Já é usuário?</span>
       </nav>
-      <section className={styles.modalHeading}>
-        <h2>Novo Usuário</h2>
-        <p>Tenha acesso a sua conta em segundos.</p>
-      </section>
-      <section className={styles.modalContent}>
-        <span className={styles.closeButton}>
-          <IoIosCloseCircleOutline onClick={onClose} />
-        </span>
-        <form className={styles.form}>
-          <label className={styles.label}>Nome</label>
-          <section className={styles.inputWrapper}>
-            <input className={styles.input} type="text" required />
-          </section>
 
-          <label className={styles.label}>Email</label>
-          <section className={styles.inputWrapper}>
-            <input
-              className={styles.input}
-              type="email"
-              value={email}
-              onChange={handleEmailChange}
-              required
-            />
-            {email && (
-              <IoClose className={styles.clearIcon} onClick={clearEmail} />
-            )}
-          </section>
+      <section className={styles.modalWrap}>
+        <section className={styles.modalHeading}>
+          <h2>Novo Usuário</h2>
+          <p>Tenha acesso a sua conta em segundos.</p>
+        </section>
 
-          <label className={styles.label}>Senha</label>
-          <section className={styles.passwordWrapper}>
-            <input
-              className={styles.input}
-              type={showPassword ? "text" : "password"}
-              required
-            />
-            {showPassword ? (
-              <LuEyeClosed className={styles.eyeIcon} onClick={toggleShowPassword} />
-            ) : (
-              <LuEye className={styles.eyeIcon} onClick={toggleShowPassword} />
-            )}
-          </section>
-          <button className={styles.button} type="submit">Cadastrar</button>
-
-          <span className={styles.spanOr}>Ou</span>
-
-          <section className={styles.socialLogin}>
-            <button className={styles.googleButton}>
-              <FcGoogle className={styles.socialIcon} />
-              Cadastrar com Google
-            </button>
-            <button className={styles.githubButton}>
-              <IoLogoGithub className={styles.socialIcon} />
-              Cadastrar com GitHub
-            </button>
-          </section>
-        </form>
+        <section className={styles.modalContent}>
+          <span className={styles.closeButton}>
+            <IoIosCloseCircleOutline onClick={onClose} />
+          </span>
+          <form className={styles.form}>
+            <label className={styles.label}>Nome</label>
+            <section className={styles.inputWrapper}>
+              <input className={styles.input} type="text" required />
+            </section>
+            <label className={styles.label}>Email</label>
+            <section className={styles.inputWrapper}>
+              <input
+                className={styles.input}
+                type="email"
+                value={email}
+                onChange={handleEmailChange}
+                required
+              />
+              {email && (
+                <IoClose className={styles.clearIcon} onClick={clearEmail} />
+              )}
+            </section>
+            <label className={styles.label}>Senha</label>
+            <section className={styles.passwordWrapper}>
+              <input
+                className={styles.input}
+                type={showPassword ? "text" : "password"}
+                required
+              />
+              {showPassword ? (
+                <LuEyeClosed className={styles.eyeIcon} onClick={toggleShowPassword} />
+              ) : (
+                <LuEye className={styles.eyeIcon} onClick={toggleShowPassword} />
+              )}
+            </section>
+            <button className={styles.button} type="submit">Cadastrar</button>
+            <span className={styles.spanOr}>Ou</span>
+            <section className={styles.socialLogin}>
+              <button className={styles.googleButton}>
+                <FcGoogle className={styles.socialIcon} />
+                Cadastrar com Google
+              </button>
+              <button className={styles.githubButton}>
+                <IoLogoGithub className={styles.socialIcon} />
+                Cadastrar com GitHub
+              </button>
+            </section>
+          </form>
+        </section>
       </section>
     </section>
   );
--- streaming-root/src/components/ModalLogin.jsx
+++ streaming-root/src/components/ModalLogin.jsx
@@ -69,62 +69,60 @@ export default function ModalLogin({
         <span onClick={handleCadastroClick}>Cadastre-se agora</span>
       </nav>
 
-      <section className={styles.modalHeading}>
-        <h2>Entrar</h2>
-        <p>Digite o endereço de e-mail e a senha da sua conta Cineminha.</p>
-      </section>
+      <section className={styles.modalWrap}>
+        <section className={styles.modalHeading}>
+          <h2>Entrar</h2>
+          <p>Digite o endereço de e-mail e a senha da sua conta Cineminha.</p>
+        </section>
 
-      <section className={styles.modalContent}>
-        <span className={styles.closeButton}>
-          <IoIosCloseCircleOutline onClick={onClose} />
-        </span>
-        <form className={styles.form}>
-          <label className={styles.label}>Email</label>
-          <section className={styles.inputWrapper}>
-            <input
-              className={styles.input}
-              type="email"
-              value={email}
-              onChange={handleEmailChange}
-              required
-            />
-            {email && (
-              <IoClose className={styles.clearIcon} onClick={clearEmail} />
-            )}
-          </section>
-          <label className={styles.label}>Senha</label>
-          <section className={styles.passwordWrapper}>
-            <input className={styles.input} type={showPassword ? "text" : "password"} required />
-            {showPassword ? (
-              <LuEyeClosed className={styles.eyeIcon} onClick={toggleShowPassword} />
-            ) : (
-              <LuEye className={styles.eyeIcon} onClick={toggleShowPassword} />
-            )}
-          </section>
-
-          <section className={styles.options}>
-            <section className={styles.remember}>
-              <input type="checkbox" id="remember" />
-              <label htmlFor="remember">Lembrar senha</label>
+        <section className={styles.modalContent}>
+          <span className={styles.closeButton}>
+            <IoIosCloseCircleOutline onClick={onClose} />
+          </span>
+          <form className={styles.form}>
+            <label className={styles.label}>Email</label>
+            <section className={styles.inputWrapper}>
+              <input
+                className={styles.input}
+                type="email"
+                value={email}
+                onChange={handleEmailChange}
+                required
+              />
+              {email && (
+                <IoClose className={styles.clearIcon} onClick={clearEmail} />
+              )}
             </section>
-            <a href="#" className={styles.forgot}>Esqueceu a senha?</a>
-          </section>
-
-          <button className={styles.button} type="submit">Entrar</button>
-
-          <span className={styles.spanOr}>Ou</span>
-
-          <section className={styles.socialLogin}>
-            <button onClick={handleGoogleLogin} className={styles.googleButton}>
-              <FcGoogle className={styles.socialIcon} />
-              Continue com Google
-            </button>
-            <button onClick={handleGitHubLogin} className={styles.githubButton}>
-              <IoLogoGithub className={styles.socialIcon} />
-              Continue com GitHub
-            </button>
-          </section>
-        </form>
+            <label className={styles.label}>Senha</label>
+            <section className={styles.passwordWrapper}>
+              <input className={styles.input} type={showPassword ? "text" : "password"} required />
+              {showPassword ? (
+                <LuEyeClosed className={styles.eyeIcon} onClick={toggleShowPassword} />
+              ) : (
+                <LuEye className={styles.eyeIcon} onClick={toggleShowPassword} />
+              )}
+            </section>
+            <section className={styles.options}>
+              <section className={styles.remember}>
+                <input type="checkbox" id="remember" />
+                <label htmlFor="remember">Lembrar senha</label>
+              </section>
+              <a href="#" className={styles.forgot}>Esqueceu a senha?</a>
+            </section>
+            <button className={styles.button} type="submit">Entrar</button>
+            <span className={styles.spanOr}>Ou</span>
+            <section className={styles.socialLogin}>
+              <button onClick={handleGoogleLogin} className={styles.googleButton}>
+                <FcGoogle className={styles.socialIcon} />
+                Continue com Google
+              </button>
+              <button onClick={handleGitHubLogin} className={styles.githubButton}>
+                <IoLogoGithub className={styles.socialIcon} />
+                Continue com GitHub
+              </button>
+            </section>
+          </form>
+        </section>
       </section>
     </section>
   );
